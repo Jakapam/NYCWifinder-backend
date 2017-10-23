88dc59d69348f895cdaad7847ecfa96457b6513d
github username <github email address>
2017-10-23 10:13:24 -0400

    fully seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,11 @@ json_data = RestClient.get 'https://data.cityofnewyork.us/api/views/varh-9tsp/ro
 
 parsed_json_data = JSON.parse(json_data)
 
-byebug
-
 puts parsed_json_data["data"]
 
-
-# Borough.find_or_create_by(name: )
+parsed_json_data['data'].each do |hotspot_data|
+  borough = Borough.find_or_create_by(name: hotspot_data[26])
+  neighborhood = Neighborhood.find_by(name: hotspot_data[28]) || Neighborhood.create(name: hotspot_data[28], borough_id: borough.id)
+  zipcode = Zipcode.find_by(zipcode_num: hotspot_data[30]) || Zipcode.create(zipcode_num: hotspot_data[30], neighborhood_id: neighborhood.id)
+  Hotspot.create(latitude: hotspot_data[15], longitude: hotspot_data[16], service_provider: hotspot_data[12], closest_address: hotspot_data[14], service_type: hotspot_data[19], service_options: hotspot_data[20], network_name: hotspot_data[22], hotspot_city_id: hotspot_data[23], zipcode_id: zipcode.id)
+end
